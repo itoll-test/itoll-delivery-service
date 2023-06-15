00d125d5b475a5630b7524d26631e3ff19086924
Seyed Milad Hashemi <s.milad.hashemi@gmail.com>
2023-06-15 21:00:08 +0330

    Confgiration is apllied to application. config module imported to app.module.ts. Unnessery files removed from codebase(app.controller,app.controller.spec,app.service)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'configs/configuration';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      cache: true,
+      validationSchema: Joi.object({
+        env: Joi.string().valid('development', 'production', 'test'),
+        port: Joi.number(),
+        userPort: Joi.number(),
+        rabbitMQ: Joi.object().keys({
+          url: Joi.string().uri(),
+          userQueue: Joi.string(),
+        }),
+        postgres: Joi.object().keys({
+          username: Joi.string(),
+          password: Joi.string(),
+          address: Joi.string().required(),
+          port: Joi.string().required(),
+          db: Joi.string().required(),
+        }),
+      }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      disableErrorMessages:
+        configService.get('env') === 'production' ? true : false,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  await app.listen(configService.get('port'), configService.get('bindIP'));
 }
 bootstrap();
