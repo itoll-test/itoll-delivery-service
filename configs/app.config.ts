3aaadc383ef57d8b2c0a3bf8465332dbaba2d5c9
Seyed Milad Hashemi <s.milad.hashemi@gmail.com>
2023-06-17 18:23:31 +0330

    Config structure is granulated

---
+++ configs/app.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { dataSourceOptions } from 'db/data-source';
+
+export default () => ({
+  env: process.env.NODE_ENV || 'development',
+  bindIP: process.env.ITOLL_BIND_IP || '0.0.0.0',
+  port: parseInt(process.env.ITOLL_PORT, 10) || 3000,
+  rabbitMQ: {
+    url: process.env.ITOLL_RABBITMQ_URL || 'amqp://localhost:5672',
+    deliveryQueue: process.env.ITOLL_RABBITMQ_QUEUE || 'itoll_delivery_queue',
+  },
+  typeORM: (): TypeOrmModuleOptions => ({
+    ...dataSourceOptions,
+    //TODO: thinkabout this
+    synchronize: process.env.NODE_ENV === 'development' ? true : false,
+    logging: process.env.NODE_ENV === 'development' ? true : false,
+  }),
+});
