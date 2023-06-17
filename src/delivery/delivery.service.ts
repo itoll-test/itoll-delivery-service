aabf006389d0a561f17ee0475f3defc9f1808d4a
Seyed Milad Hashemi <s.milad.hashemi@gmail.com>
2023-06-17 18:24:29 +0330

    Entites added and delivery module updated.

--- src/delivery/delivery.service.ts
+++ src/delivery/delivery.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDeliveryDto } from './dto/create-delivery.dto';
 import { UpdateDeliveryDto } from './dto/update-delivery.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Delivery } from './entities/delivery.entity';
+import { Repository } from 'typeorm';
+import { Consignment } from './entities/consignment.entity';
 
 @Injectable()
 export class DeliveryService {
+  constructor(
+    @InjectRepository(Delivery)
+    private deliveryRepository: Repository<Delivery>,
+    @InjectRepository(Consignment)
+    private consignmentRepository: Repository<Consignment>,
+  ) {}
+
   create(createDeliveryDto: CreateDeliveryDto) {
     return 'This action adds a new delivery';
   }
